f691d1c8465395f6c103e570d6c9c8139a09c2ad
common-cold <prajjwalkohli@gmail.com>
2024-05-23 00:07:33 +0530

    Adding dependency for JSONObject

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -11,6 +13,24 @@ public class Main {
         try {
             URL url = new URL("https://api.cuvora.com/car/partner/cricket-data");
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setRequestProperty("apiKey", "test-creds@2320");
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            StringBuffer result = new StringBuffer();
+            while((line = input.readLine()) != null){
+                result.append(line);
+            }
+
+            input.close();
+
+            Jso
+
+            System.out.println(result);
+
 
 
 
