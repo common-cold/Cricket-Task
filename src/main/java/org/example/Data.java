ce7a335b37104f3ccbb14166a862df09be07ba4d
common-cold <prajjwalkohli@gmail.com>
2024-05-23 13:44:31 +0530

    > Using okHTTP for GET request > Using ObjectMapper to get parse JSON data > Added validations and error handlings

---
+++ src/main/java/org/example/Data.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Data {
+    public ArrayList<Details> data;
+
+    public ArrayList<Details> getData() {
+        return data;
+    }
+
+    public void setData(ArrayList<Details> data) {
+        this.data = data;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,75 +1,83 @@
 package org.example;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Objects;
 
 
 public class Main {
-    public static JSONObject executeURL(String inputURL){
-        try{
-            URL url=new URL(inputURL);
-            HttpURLConnection connection=(HttpURLConnection)url.openConnection();
 
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("Content-Type","application/json");
-            connection.setRequestProperty("apiKey","test-creds@2320");
+    public static String executeURL(String inputURL){
+        OkHttpClient httpClient = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(inputURL)
+                .addHeader("Content-Type", "application/json")
+                .addHeader("apiKey","test-creds@2320")
+                .build();
+        try {
+            Response response = httpClient.newCall(request).execute();
 
-            BufferedReader input=new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String line;
-            StringBuffer result=new StringBuffer();
-            while((line=input.readLine())!=null){
-                result.append(line);
-            }
+            //Check if the response is valid
+            if(response.code() != 200)
+                throw new RuntimeException(response.message());
 
-            input.close();
-            connection.disconnect();
-
-            JSONObject jsonObject=new JSONObject(result.toString());
-
-            return jsonObject;
-
-        }catch(IOException e){
+            return response.body().string();
+        }
+        catch (IOException e) {
             throw new RuntimeException(e);
         }
+
     }
 
     public static int scoreConverter(String score){
         return Integer.valueOf(score.split("/")[0]);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JsonProcessingException {
         int maxScore = -1;
         String teamName = null;
         int count = 0;
         String url = "https://api.cuvora.com/car/partner/cricket-data";
-        JSONArray response = executeURL(url).getJSONArray("data");
+        String response = executeURL(url);
 
-        for(int i=0; i< response.length(); i++){
-            JSONObject res = (JSONObject) response.get(i);
-            int s1 = scoreConverter(res.get("t1s").toString());
-            int s2 = scoreConverter(res.get("t2s").toString());
-            int maxi = Integer.max(s1, s2);
-            if(maxi > maxScore){
-                maxScore = maxi;
-                if(s1 >= s2){
-                    teamName = res.get("t1").toString();
+        ObjectMapper objectMapper = new ObjectMapper();
+        Data data = objectMapper.readValue(response, Data.class);
+        ArrayList<Details> result = data.getData();
+
+        for(int i=0; i< result.size(); i++){
+            Details res = result.get(i);
+
+            String score1 = res.getT1s();
+            String score2 = res.getT2s();
+            String team1 = res.getT1();
+            String team2 = res.getT2();
+
+            //Validate scores from the response
+            if(Objects.nonNull(score1) && !"".equalsIgnoreCase(score1)
+                    && Objects.nonNull(score2) && !"".equalsIgnoreCase(score2)) {
+
+                int s1 = scoreConverter(score1);
+                int s2 = scoreConverter(score2);
+                int maxi = Integer.max(s1, s2);
+                if(maxi > maxScore){
+                    maxScore = maxi;
+
+                   //Validate team names from response
+                    if(s1 >= s2)
+                        teamName = (Objects.nonNull(team1) && !"".equalsIgnoreCase(team1)) ? team1 : " ";
+                    else
+                        teamName = (Objects.nonNull(team2) && !"".equalsIgnoreCase(team2)) ? team2 : " ";
                 }
-                else{
-                    teamName = res.get("t2").toString();
-                }
+                if(s1+s2 >= 300)
+                    count++;
             }
-            if(s1+s2 >= 300)
-                count++;
         }
-
         System.out.println("Highest Score : " + maxScore + " and Team Name is : " + teamName);
         System.out.println("Number Of Matches with total 300 Plus Score : " + count);
 
